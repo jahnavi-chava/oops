e7268072af2df3a50a96945a5221230107fc3ccb
jahnavi-chava <jahnavichava38@gmail.com>
2025-03-09 14:08:29 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,8 @@
+class Example1 {
+        public static void main(String[] args) {
+                System.out.println("name: Jahnavi");
+                System.out.println("roll no: 24035");
+                System.out.println("class: CSE");
+                System.out.printl n("Section: CSEA");      
+}
+}
