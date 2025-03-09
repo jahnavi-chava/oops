a5beae0c36b7308e05603ec38e2ecff77612d8fe
jahnavi-chava <jahnavichava38@gmail.com>
2025-03-09 14:12:21 +0530

    Add files via upload

---
+++ Jahnavi.java
@@ -0,0 +1,40 @@
+class Jahnavi{
+	public static void main(String[] args){
+	String name="Jahnavi";{
+	System.out.println("Name is"+name);
+	}
+	char c='A';{
+	System.out.println("Section="+c);
+	}
+	String b ="CSE";{
+	System.out.println("classs="+b);
+	}
+	int a=35;{
+	System.out.println("Roll no="+a);
+	}
+	int tel=80;{
+	System.out.println("Telugu marks="+tel);
+	}
+	int hin=80;{
+	System.out.println("Hindi marks="+hin);
+	}
+	int eng=90;{
+	System.out.println("English marks="+eng);
+	}
+	float mat=(float)85.5;{
+	System.out.println("Math marks="+mat);
+	}
+	float sci=(float)79.9;{
+	System.out.println("science marks="+sci);
+	}
+	float soc=(float)90.5;{
+	System.out.println("social marks="+soc);
+	}
+	float sum=tel+hin+eng+mat+sic+soc;{
+	System.out.println("Total Marks="+sum);
+	}
+	float avg=sum/6;{
+	System.out.println("Average Marks="+avg);
+	}
+}
+}
